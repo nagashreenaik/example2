6aef8fab2ced85d3a79e9a64d7582ebd1e043743
nagashreenaik <nagashreenaik456@gmail.com>
2023-02-06 10:43:32 +0530

    commit program

---
+++ src/example3/example3.java
@@ -0,0 +1,14 @@
+package example3;
+
+public class example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		if(x<6)
+			System.out.println("x is smaller");
+		else
+			System.out.println("x is larger");
+	}
+
+}
